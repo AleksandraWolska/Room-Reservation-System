7f81cb212835290932e73955f77f909ab15c40a3
Aleksandra <aleksandra.joanna.wolska@gmail.com>
2023-05-07 21:07:49 +0200

    added swagger notes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import LoginPage from './login/LoginPage';
+import ListComponent from './components/ListComponent';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+
+      {/* <LoginPage /> */}
+
+      <ListComponent />
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -18,7 +24,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
