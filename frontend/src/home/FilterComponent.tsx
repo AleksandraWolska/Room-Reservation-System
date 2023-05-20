2fa6dd17aed5eb385feb6b2eb6e9c09f6fe1a019
Aleksandra <aleksandra.joanna.wolska@gmail.com>
2023-05-20 19:25:54 +0200

    checkpoint

--- frontend/src/home/FilterComponent.tsx
+++ frontend/src/home/FilterComponent.tsx
@@ -1,6 +1,6 @@
 import React, { useState, ChangeEvent } from 'react';
 import { RoomControllerApi, RoomFilterRequest, RoomFilterRequestRoomTypeEnum } from '../services/openapi';
-import { TextField, FormControl, InputLabel, Select, MenuItem, Checkbox, FormControlLabel, Button } from '@mui/material';
+import { TextField, FormControl, InputLabel, Select, MenuItem, Checkbox, FormControlLabel, Button, Grid } from '@mui/material';
 
 interface FilterComponentProps {
     onFormSubmit: (values: RoomFilterRequest) => void;
@@ -31,16 +31,6 @@ const useFormFields = (initialState: RoomFilterRequest) => {
     return { formValues, setFormValues, handleInputChange, handleSelectChange, handleCheckboxChange };
 };
 
-const resettingState: RoomFilterRequest = {
-    from: undefined,
-    to: undefined,
-    buildingName: undefined,
-    roomType: undefined,
-    isProjector: undefined,
-    minPlaces: undefined,
-    maxPlaces: undefined,
-};
-
 export const FilterComponent: React.FC<FilterComponentProps> = ({ onFormSubmit }) => {
     const { formValues, setFormValues, handleInputChange, handleSelectChange, handleCheckboxChange } = useFormFields(initialState);
 
@@ -50,96 +40,110 @@ export const FilterComponent: React.FC<FilterComponentProps> = ({ onFormSubmit }
     };
 
     const handleReset = () => {
-        setFormValues({...resettingState});
         setFormValues(initialState);
-
-        console.log(formValues)
     };
-
 
     return (
         <div>
             <h1>Filter</h1>
             <form onSubmit={handleSubmit}>
-                <TextField
-                    id="from"
-                    name="from"
-                    label="From"
-                    type="datetime-local"
-                    value={formValues.from}
-                    onChange={handleInputChange}
-                />
-                <TextField
-                    id="to"
-                    name="to"
-                    label="To"
-                    type="datetime-local"
-                    value={formValues.to}
-                    onChange={handleInputChange}
-                />
-                <TextField
-                    id="buildingName"
-                    name="buildingName"
-                    label="Building Name"
-                    value={formValues.buildingName}
-                    onChange={handleInputChange}
-                />
-
-                <FormControl>
-                    <InputLabel id="roomType-label">Room Type</InputLabel>
-                    <Select
-                        id="roomType"
-                        name="roomType"
-                        labelId="roomType-label"
-                        value={formValues.roomType}
-                        onChange={handleSelectChange}
-                    >
-                        <MenuItem onClick={() => setFormValues(prevState => ({
-                            ...(Object.fromEntries(
-                                Object.entries(prevState).filter(([key]) => key !== "roomType")
-                            ))}))}>None</MenuItem>
-                        <MenuItem value={RoomFilterRequestRoomTypeEnum.ComputersRoom}>Computer Room</MenuItem>
-                        <MenuItem value={RoomFilterRequestRoomTypeEnum.Office}>Office</MenuItem>
-                        <MenuItem value={RoomFilterRequestRoomTypeEnum.Workshop}>Workshop</MenuItem>
-                        <MenuItem value={RoomFilterRequestRoomTypeEnum.ChemistryLaboratory}>Chemistry Lab</MenuItem>
-                    </Select>
-                </FormControl>
-
-                <FormControlLabel
-                    control={
-                        <Checkbox
-                            id="isProjector"
-                            name="isProjector"
-                            checked={formValues.isProjector}
-                            onChange={handleCheckboxChange}
+                <Grid container spacing={3}>
+                    <Grid item xs={12} sm={6}>
+                        <TextField
+                            fullWidth
+                            id="from"
+                            name="from"
+                            label="From"
+                            type="datetime-local"
+                            value={formValues.from}
+                            onChange={handleInputChange}
                         />
-                    }
-                    label="Is Projector"
-                />
-
-                <TextField
-                    id="minPlaces"
-                    name="minPlaces"
-                    label="Minimum Places"
-                    type="number"
-                    value={formValues.minPlaces}
-                    onChange={handleInputChange}
-                />
-                <TextField
-                    id="maxPlaces"
-                    name="maxPlaces"
-                    label="Maximum Places"
-                    type="number"
-                    value={formValues.maxPlaces}
-                    onChange={handleInputChange}
-                />
-
-                <Button type="submit" variant="contained" color="primary">
-                    Szukaj
-                </Button>
-                <Button type="button" variant="contained" color="secondary" onClick={handleReset}>
-                Reset
-            </Button>
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <TextField
+                            fullWidth
+                            id="to"
+                            name="to"
+                            label="To"
+                            type="datetime-local"
+                            value={formValues.to}
+                            onChange={handleInputChange}
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                            fullWidth
+                            id="buildingName"
+                            name="buildingName"
+                            label="Building Name"
+                            value={formValues.buildingName}
+                            onChange={handleInputChange}
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <FormControl fullWidth>
+                            <InputLabel id="roomType-label">Room Type</InputLabel>
+                            <Select
+                                id="roomType"
+                                name="roomType"
+                                labelId="roomType-label"
+                                value={formValues.roomType}
+                                onChange={handleSelectChange}
+                            >
+                                <MenuItem value="">None</MenuItem>
+                                <MenuItem value={RoomFilterRequestRoomTypeEnum.ComputersRoom}>Computer Room</MenuItem>
+                                <MenuItem value={RoomFilterRequestRoomTypeEnum.Office}>Office</MenuItem>
+                                <MenuItem value={RoomFilterRequestRoomTypeEnum.Workshop}>Workshop</MenuItem>
+                                <MenuItem value={RoomFilterRequestRoomTypeEnum.ChemistryLaboratory}>Chemistry Lab</MenuItem>
+                            </Select>
+                        </FormControl>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <FormControlLabel
+                            control={
+                                <Checkbox
+                                    id="isProjector"
+                                    name="isProjector"
+                                    checked={formValues.isProjector}
+                                    onChange={handleCheckboxChange}
+                                />
+                            }
+                            label="Is Projector"
+                        />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <TextField
+                            fullWidth
+                            id="minPlaces"
+                            name="minPlaces"
+                            label="Minimum Places"
+                            type="number"
+                            value={formValues.minPlaces}
+                            onChange={handleInputChange}
+                        />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <TextField
+                            fullWidth
+                            id="maxPlaces"
+                            name="maxPlaces"
+                            label="Maximum Places"
+                            type="number"
+                            value={formValues.maxPlaces}
+                            onChange={handleInputChange}
+                        />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <Button fullWidth type="submit" variant="contained" color="primary">
+                            Szukaj
+                        </Button>
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <Button fullWidth type="button" variant="contained" color="secondary" onClick={handleReset}>
+                            Reset
+                        </Button>
+                    </Grid>
+                </Grid>
             </form>
         </div>
     );
--- frontend/src/home/Home.tsx
+++ frontend/src/home/Home.tsx
@@ -1,189 +1,115 @@
 import { Button, Typography, Dialog, DialogTitle, DialogActions, Grid, Radio, RadioGroup, FormControlLabel, FormControl, Container } from '@mui/material';
-import FilterComponent from "./FilterComponent";
-import BuildingsList from "../components/BuildingList";
-import RoomsList from "../components/RoomList";
-import { FilterRequest, ReservationControllerApi, Room, RoomControllerApi, RoomFilterRequest } from "../services/openapi";
-import RoomListView from "../views/RoomListView";
 import React, { useState, useEffect } from 'react';
-import { BuildingControllerApi } from '../services/openapi/apis/BuildingControllerApi';
-import { BuildingWithRoomsResponse } from '../services/openapi/models/BuildingWithRoomsResponse';
-import BuildingWithRoomsListView from "../views/BuildingWithRoomsListView";
-import ReservationView from "../views/ReservationView";
-import BuildingWithRoomsMapView from '../views/BuildingWithRoomsMapView';
+import { BuildingControllerApi, FilterRequest, ReservationControllerApi, Room, RoomControllerApi, RoomFilterRequest } from '../services/openapi';
 
-const modes = {
-    ROOMS: "rooms",
-    BUILDINGS_WITH_ROOMS_MAP: "buildingsWithRoomsMap",
-    BUILDINGS_WITH_ROOMS_LIST: "buildingsWithRoomsList"
+import FilterComponent from "./FilterComponent";
+import BuildingWithRoomsMapView from '../views/BuildingWithRoomsMapView';
+import BuildingWithRoomsListView from "../views/BuildingWithRoomsListView";
+import RoomListView from "../views/RoomListView";
+import ReservationView from "../views/ReservationView";
+
+import { BuildingWithRoomsResponse } from '../services/openapi/models/BuildingWithRoomsResponse';
+
+enum ViewMode {
+  Rooms = "rooms",
+  BuildingsWithRoomsMap = "buildingsWithRoomsMap",
+  BuildingsWithRoomsList = "buildingsWithRoomsList"
 }
 
 interface HomeProps {
-    userId: number;
-  }
+  userId: number;
+}
+
+const Home: React.FC<HomeProps> = ({ userId }) => {
+  const roomApi = new RoomControllerApi();
+  const reservationApi = new ReservationControllerApi();
   
-  const Home: React.FC<HomeProps> = ({ userId }) => {
-    const roomApi = new RoomControllerApi();
-    const reservationApi = new ReservationControllerApi();
-    const [displayMode, setDisplayMode] = useState(modes.ROOMS)
-    const [displayedRoomsList, setDisplayedRoomsList] = useState<Room[]>([])
-    const [buildings, setBuildings] = useState<BuildingWithRoomsResponse[]>([]);
+  const [displayMode, setDisplayMode] = useState(ViewMode.Rooms);
+  const [displayedRoomsList, setDisplayedRoomsList] = useState<Room[]>([]);
+  const [buildings, setBuildings] = useState<BuildingWithRoomsResponse[]>([]);
+  const [showDialog, setShowDialog] = useState(false);
+  const [dialogContent, setDialogContent] = useState(''); // to hold either "successful" or "conflict"
+  const [chosenRoom, setChosenRoom] = useState<number>()
+  const [view, setView] = useState(ViewMode.BuildingsWithRoomsMap);
 
-
-    const [showDialog, setShowDialog] = useState(false);
-    const [dialogContent, setDialogContent] = useState(''); // to hold either "successful" or "conflict"
-
-
-    const [chosenRoom, setChosenRoom] = useState<number>()
-
-
-
-
-    useEffect(() => {
-        const fetchBuildings = async () => {
-            const api = new BuildingControllerApi();
-            const buildingsData = await api.allWithRooms();
-            setBuildings(buildingsData);
-        }
-
-        fetchBuildings();
-    }, []);
-
-
-
-    const handleFormSubmit = (values: RoomFilterRequest) => {
-
-        const requestParameters: FilterRequest = {
-            roomFilterRequest: values,
-        };
-        console.log("przekazano")
-        console.log(requestParameters)
-        roomApi.filter(requestParameters)
-            .then(response => {
-                setDisplayedRoomsList(response)
-                console.log(response);
-            })
-            .catch(error => {
-                console.error(error);
-            });
-    };
-
-
-    const handleReservation = async (dates: (Date | undefined)[], roomId: number) => {
-        try {
-            const validDates = dates.filter(date => date !== undefined) as Date[]; // Ensure there are no undefined dates in the array.
-
-            const response = await reservationApi.reserve({
-                roomId: roomId,
-                userId: userId,
-                requestBody: validDates,
-            });
-
-            console.log("odpowiedz po rezerwacji")
-            console.log(response); // Log the response for debugging purposes.
-
-            setDialogContent('Successful');
-            setShowDialog(true);
-
-
-
-        } catch (error: any) {
-            if (error.status === 409) {
-                setDialogContent('Conflict');
-                setShowDialog(true);
-            } else {
-                // handle other errors
-                console.error(error);
-            }
-        }
-    };
-
-
-    const closeDialog = () => {
-        setShowDialog(false);
+  useEffect(() => {
+    const fetchBuildings = async () => {
+      const api = new BuildingControllerApi();
+      const buildingsData = await api.allWithRooms();
+      setBuildings(buildingsData);
     }
+    fetchBuildings();
+  }, []);
 
+  const handleFormSubmit = async (values: RoomFilterRequest) => {
+    const requestParameters: FilterRequest = { roomFilterRequest: values };
+    try {
+      const response = await roomApi.filter(requestParameters);
+      setDisplayedRoomsList(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
+  const handleReservation = async (dates: (Date | undefined)[], roomId: number) => {
+    const validDates = dates.filter(date => date !== undefined) as Date[]; // Ensure there are no undefined dates in the array.
+    try {
+      const response = await reservationApi.reserve({ roomId, userId, requestBody: validDates });
+      setDialogContent('Successful');
+      setShowDialog(true);
+    } catch (error: any) {
+      if (error.status === 409) {
+        setDialogContent('Conflict');
+        setShowDialog(true);
+      } else {
+        console.error(error);
+      }
+    }
+  };
 
-    const [view, setView] = useState(modes.BUILDINGS_WITH_ROOMS_MAP);
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setView(event.target.value as ViewMode);
+  };
 
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setView((event.target as HTMLInputElement).value);
-    };
-
-    // ... your useEffect and handle functions ...
-
-    let RightComponent;
+  const RightComponent = (() => {
+    const commonProps = { onRoomSelect: (room: any) => setChosenRoom(room.number) };
     switch (view) {
-        case modes.BUILDINGS_WITH_ROOMS_MAP:
-            RightComponent = <BuildingWithRoomsMapView buildings={buildings} />;
-            break;
-        case modes.BUILDINGS_WITH_ROOMS_LIST:
-            RightComponent = <BuildingWithRoomsListView buildings={buildings} onRoomSelect={(room) => {
-                console.log("You selected room number: ", room.number);
-                setChosenRoom(room.number)
-            }} />;
-            break;
-        case modes.ROOMS:
-            RightComponent = <RoomListView rooms={displayedRoomsList} onRoomSelect={(room) => {
-                console.log("You selected room number: ", room.number);
-                setChosenRoom(room.number)
-            }} />;
-            break;
-        default:
-            RightComponent = null;
+      case ViewMode.BuildingsWithRoomsMap: return <BuildingWithRoomsMapView buildings={buildings} />;
+      case ViewMode.BuildingsWithRoomsList: return <BuildingWithRoomsListView {...commonProps} buildings={buildings} />;
+      case ViewMode.Rooms: return <RoomListView {...commonProps} rooms={displayedRoomsList} />;
+      default: return null;
     }
+  })();
 
-    return (
-        <div>
-            
-            <Container>
-                <Grid container spacing={2}>
-                    <Grid item xs={12} md={3}>
-                        <Button onClick={() => setView(modes.BUILDINGS_WITH_ROOMS_MAP)}>Widok budynków</Button>
-                        <Button onClick={() => setView(modes.ROOMS)}>Widok pokoi</Button>
-
-                        {view.includes("buildings") && (
-                            <FormControl component="fieldset">
-                                <RadioGroup aria-label="view" name="view" value={view} onChange={handleChange}>
-                                    <FormControlLabel value={modes.BUILDINGS_WITH_ROOMS_MAP} control={<Radio />} label="Map View" />
-                                    <FormControlLabel value={modes.BUILDINGS_WITH_ROOMS_LIST} control={<Radio />} label="List View" />
-                                </RadioGroup>
-                            </FormControl>
-                        )}
-
-                        {view === modes.ROOMS && <FilterComponent onFormSubmit={handleFormSubmit} />}
-                    </Grid>
-                    <Grid item xs={12} md={9}>
-                        {chosenRoom &&
-                            <ReservationView id={chosenRoom} onReservation={handleReservation} />}
-
-                        <Dialog open={showDialog} onClose={closeDialog}>
-                            <DialogTitle>{dialogContent}</DialogTitle>
-                            <DialogActions>
-                                <Button onClick={closeDialog} color="primary">
-                                    OK
-                                </Button>
-                            </DialogActions>
-                        </Dialog>
-                        {RightComponent}
-                    </Grid>
-                </Grid>
-            </Container>
-
-
-        </div>
-    );
+  return (
+    <Container>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={3}>
+          <Button onClick={() => setView(ViewMode.BuildingsWithRoomsMap)}>Widok budynków</Button>
+          <Button onClick={() => setView(ViewMode.Rooms)}>Widok pokoi</Button>
+          {view.includes("buildings") && (
+            <FormControl component="fieldset">
+              <RadioGroup aria-label="view" name="view" value={view} onChange={handleChange}>
+                <FormControlLabel value={ViewMode.BuildingsWithRoomsMap} control={<Radio />} label="Map View" />
+                <FormControlLabel value={ViewMode.BuildingsWithRoomsList} control={<Radio />} label="List View" />
+              </RadioGroup>
+            </FormControl>
+          )}
+          {view === ViewMode.Rooms && <FilterComponent onFormSubmit={handleFormSubmit} />}
+        </Grid>
+        <Grid item xs={12} md={9}>
+          {chosenRoom && <ReservationView id={chosenRoom} onReservation={handleReservation} />}
+          <Dialog open={showDialog} onClose={() => setShowDialog(false)}>
+            <DialogTitle>{dialogContent}</DialogTitle>
+            <DialogActions>
+              <Button onClick={() => setShowDialog(false)} color="primary">OK</Button>
+            </DialogActions>
+          </Dialog>
+          {RightComponent}
+        </Grid>
+      </Grid>
+    </Container>
+  );
 };
 
 export default Home;
-
-
-
-
-
-
-
-
-
-
-
