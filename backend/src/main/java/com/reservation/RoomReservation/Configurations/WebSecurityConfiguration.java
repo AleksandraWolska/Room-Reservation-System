b7ee295b37ac59f00a545cead38049c47d3ea1b8
szymi <shibek123@gamil.com>
2023-05-23 17:51:13 +0200

    confused commit

--- backend/src/main/java/com/reservation/RoomReservation/Configurations/WebSecurityConfiguration.java
+++ backend/src/main/java/com/reservation/RoomReservation/Configurations/WebSecurityConfiguration.java
@@ -2,11 +2,17 @@ package com.reservation.RoomReservation.Configurations;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -14,25 +20,38 @@ public class WebSecurityConfiguration {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf()
-            .disable()
-            .authorizeHttpRequests()
+        http.csrf().disable()
+            .cors()
+            .and().authorizeHttpRequests()
             .requestMatchers("/swagger-ui/**", "/v1/**", "/v2/**", "/v3/**", "/swagger/**").permitAll()
-            .requestMatchers("/reservation/users/*","/reservation/users/register").permitAll()
+            .requestMatchers("/reservation/users/*","/reservation/users/register", "/login").permitAll()
             .anyRequest().authenticated()
-            .and()
-            .formLogin()
-            .loginProcessingUrl("/perform_login")
-            .defaultSuccessUrl("/reservation/users/", true)
-            .and()
-            .logout()
-            .logoutUrl("/perform_logout")
+            .and().oauth2Login().loginProcessingUrl("http://localhost:3000/")
+            .defaultSuccessUrl("http://localhost:3000/", true)
+            .successHandler(myAuthenticationSuccessHandler())
+            .and().logout()
+            .logoutUrl("/logout").logoutSuccessUrl("/")
+            .clearAuthentication(true)
+            .invalidateHttpSession(true)
+            .deleteCookies("JSESSIONID")
+            .and().formLogin()
+            .loginProcessingUrl("/login")
+            .defaultSuccessUrl("http://localhost:3000/", true)
+            .successHandler(myAuthenticationSuccessHandler()).and().sessionManagement().enableSessionUrlRewriting(true)
+            .and().logout()
+            .logoutUrl("/logout").logoutSuccessUrl("/")
+            .clearAuthentication(true)
+            .invalidateHttpSession(true)
             .deleteCookies("JSESSIONID")
             .and().httpBasic();
 
         return http.build();
     }
 
+    @Bean
+    public AuthenticationSuccessHandler myAuthenticationSuccessHandler(){
+        return new MySimpleUrlAuthenticationSuccessHandler();
+    }
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
--- backend/src/main/java/com/reservation/RoomReservation/Controllers/UserController.java
+++ backend/src/main/java/com/reservation/RoomReservation/Controllers/UserController.java
@@ -5,8 +5,13 @@ import com.reservation.RoomReservation.Repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -31,5 +36,15 @@ public class UserController {
         userRepository.save(user);
 
         return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @GetMapping("/user")
+    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+        return Collections.singletonMap("name", principal.getAttribute("name"));
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<String> success(){
+        return new ResponseEntity<>("success", HttpStatus.OK);
     }
 }
