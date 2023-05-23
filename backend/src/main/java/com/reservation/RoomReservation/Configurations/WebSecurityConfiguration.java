5e336f67af53846912794ea5a580ad7681b75d1b
szymi <shibek123@gamil.com>
2023-05-23 21:33:43 +0200

    database add oauth2 users

--- backend/src/main/java/com/reservation/RoomReservation/Configurations/WebSecurityConfiguration.java
+++ backend/src/main/java/com/reservation/RoomReservation/Configurations/WebSecurityConfiguration.java
@@ -1,17 +1,31 @@
 package com.reservation.RoomReservation.Configurations;
 
+import com.reservation.RoomReservation.Services.MyUserDetailsService;
+import com.reservation.RoomReservation.Utils.MyOAuth2User;
+import com.reservation.RoomReservation.Utils.UserService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfiguration {
+
+    @Autowired
+    private UserService userService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -23,6 +37,15 @@ public class WebSecurityConfiguration {
                 .anyRequest().authenticated()
             .and().oauth2Login()
                 .defaultSuccessUrl("http://localhost:3000/", true)
+                .successHandler(new AuthenticationSuccessHandler() {
+                    @Override
+                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                                        Authentication authentication) throws IOException, ServletException {
+                        MyOAuth2User oauthUser = new MyOAuth2User((OAuth2User) authentication.getPrincipal());
+                        userService.processOAuthPostLogin(oauthUser.getEmail());
+
+                        response.sendRedirect("http://localhost:3000/");
+                    };})
             .and().formLogin()
                 .loginProcessingUrl("/login")
                 .defaultSuccessUrl("http://localhost:3000/", true)
@@ -39,6 +62,8 @@ public class WebSecurityConfiguration {
 
         return http.build();
     }
+
+
 
     @Bean
     public AuthenticationSuccessHandler myAuthenticationSuccessHandler(){
