546e89d2bde80f4158312d88e322bdee1c3064ff
szymi <shibek123@gamil.com>
2023-05-23 17:49:01 +0200

    clear security imports

--- backend/src/main/java/com/reservation/RoomReservation/Configurations/WebSecurityConfiguration.java
+++ backend/src/main/java/com/reservation/RoomReservation/Configurations/WebSecurityConfiguration.java
@@ -2,17 +2,12 @@ package com.reservation.RoomReservation.Configurations;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 
 @Configuration
 @EnableWebSecurity
